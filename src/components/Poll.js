5250bb69869effcd59fa7b4fdfb9ea8e3bf29090
Parikshith <pkedilay@asu.edu>
2020-05-31 13:50:20 -0700

    Routing to Results for answered questions

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -4,25 +4,46 @@ import Header from './Header'
 import { connect } from 'react-redux'
 
 class Poll extends Component {
+    state = {
+        answered : false,
+    }
+    componentDidMount =() => {
+        const {authUser, id} = this.props
+        if (Object.keys(authUser.answers).includes(id)) {
+            this.setState({
+                answered : true,
+            })
+        }
+    }
     render() {
         return (
             <div>
                 <Header />
                 <div className="col-md-6 col-md-offset-3">
-                    <Card 
+                    {!this.state.answered &&(
+                        <Card 
                         id={this.props.id}
                         q={true}
-                    />
+                        />
+                    )}
+                    
+                    {this.state.answered &&(
+                        <Card 
+                        id={this.props.id}
+                        result={true}
+                        />
+                    )}
                 </div>
             </div>
         )
     }
 }
-function mapStateToProps({users, questions}, props) {
+function mapStateToProps({users, questions, authUser}, props) {
     const { id } = props.match.params
     return {
         id, 
-
+        users,
+        authUser,
     }
 }
 export default connect(mapStateToProps)(Poll)
