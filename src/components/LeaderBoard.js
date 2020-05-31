e8a226a76cddff174109c3ed47b2a5615e705c94
Parikshith <pkedilay@asu.edu>
2020-05-30 23:42:28 -0700

    Created Leaderboard UI

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,14 +1,68 @@
 import React, { Component } from 'react'
 import Header from './Header'
+import { connect } from 'react-redux'
 
 class LeaderBoard extends Component {
     render() {
         return (
             <div>
                 <Header />
+                {this.props.users.map(user => (
+                <div key={user.id} className="panel panel-default col-md-6 col-md-offset-3">
+                    <div className="panel-body">
+                        
+                            <div>
+                                <div className="col-md-3" >
+                                    <img className="img-circle" width="100%" src={user.avatarURL} alt="User avatar" />
+                                </div>
+                                <div className="col-md-6 left right" >
+                                    <h3>{user.name}</h3>
+                                    <hr />
+                                    <div className="row">
+                                        <div className="col-md-9">
+                                            <label>Answered Questions</label> 
+                                        </div>
+                                        <div className="col-md-3 text-right">
+                                            <label>{Object.keys(user.answers).length}</label>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <div className="row">
+                                        <div className="col-md-9">
+                                            <label>Created Questions</label>
+                                        </div>
+                                        <div className="col-md-3 text-right">
+                                            <label>{user.questions.length}</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="panel panel-default" >
+                                        <div className="panel-heading text-center">
+                                            <label>Score</label>    
+                                        </div>
+                                        <div className="panel-body">
+                                            <div className="custom-circle">
+                                            {Object.keys(user.answers).length + user.questions.length}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                    </div>
+                </div>
+                ))}
             </div>
         )
     }
 }
 
-export default LeaderBoard
+
+function mapStateToProps({users}) {
+    return {
+        users: Object.values(users)
+        
+    }
+}
+export default connect(mapStateToProps)(LeaderBoard)
